a69906c4ddbbd833ecf676fe32648149a55ac481
Helge Waastad <helge.waastad@datametrix.no>
2014-05-22 21:54:37 +0200

    Logout session on secure

--- src/main/java/org/waastad/principallookup/controller/ViewController.java
+++ src/main/java/org/waastad/principallookup/controller/ViewController.java
@@ -5,11 +5,15 @@
  */
 package org.waastad.principallookup.controller;
 
+import java.io.IOException;
 import java.io.Serializable;
 import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.inject.Named;
-import org.omnifaces.cdi.ViewScoped;
+import javax.servlet.ServletException;
 import org.waastad.principallookup.ejb.RegisterServiceLocal;
 
 /**
@@ -17,7 +21,7 @@ import org.waastad.principallookup.ejb.RegisterServiceLocal;
  * @author Helge Waastad <helge.waastad@datametrix.no>
  */
 @Named
-@ViewScoped
+@SessionScoped
 public class ViewController implements Serializable {
 
     private static final long serialVersionUID = -5993764783281077184L;
@@ -29,10 +33,18 @@ public class ViewController implements Serializable {
         System.out.println("ViewController: doing priviledged action");
         registerServiceLocal.doPrivilegedStuff();
     }
-    
+
     public void unPriviledgedAction(ActionEvent event) {
         System.out.println("ViewController: doing un-priviledged action");
         registerServiceLocal.doUnprivilegedStuff();
+    }
+
+    public void logout(ActionEvent event) throws ServletException, IOException {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        ExternalContext externalContext = facesContext.getExternalContext();
+        externalContext.invalidateSession();
+        externalContext.responseSendError(401, "You are logged out.");
+        facesContext.responseComplete();
     }
 
 }
